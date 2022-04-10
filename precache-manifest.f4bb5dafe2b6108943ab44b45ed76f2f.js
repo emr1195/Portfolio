e17ce620186780e1321972cf7a71fd25abd6bd85
Enoch Mena <enockm1120@gmail.com>
2022-04-10 15:04:57 -0500

    Updates

---
+++ precache-manifest.f4bb5dafe2b6108943ab44b45ed76f2f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f542c387743f6cd03dcb424844a3ff71",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5a85784c2da887934813",
+    "url": "/Portfolio/static/css/2.998bdad5.chunk.css"
+  },
+  {
+    "revision": "5a85784c2da887934813",
+    "url": "/Portfolio/static/js/2.30ffa36b.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/Portfolio/static/js/2.30ffa36b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a344a2032c0040495ae7",
+    "url": "/Portfolio/static/js/main.d1bcbddf.chunk.js"
+  },
+  {
+    "revision": "fd11c46505104759cf2b",
+    "url": "/Portfolio/static/js/runtime-main.b9da9406.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/Portfolio/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/Portfolio/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/Portfolio/static/media/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "b5f19e4384bf5a797f68f904dea67d5f",
+    "url": "/Portfolio/static/media/hero.b5f19e43.jpg"
+  },
+  {
+    "revision": "be307f310b52a97c82cb8182d81b97a2",
+    "url": "/Portfolio/static/media/mach 1 1969.be307f31.png"
+  },
+  {
+    "revision": "5167bee51a6f5a7040bd8383d44cde82",
+    "url": "/Portfolio/static/media/map.5167bee5.png"
+  },
+  {
+    "revision": "3c8124aa30d61bdfc84bd260afd61026",
+    "url": "/Portfolio/static/media/me.3c8124aa.jpg"
+  },
+  {
+    "revision": "980062219c948b04925365a17bebb9b4",
+    "url": "/Portfolio/static/media/p1.98006221.png"
+  },
+  {
+    "revision": "34d9b02bba548dfcc2ac569fb3f96e8d",
+    "url": "/Portfolio/static/media/p2.34d9b02b.png"
+  },
+  {
+    "revision": "23b772adc0820200fd1440f64f011db9",
+    "url": "/Portfolio/static/media/p3.23b772ad.png"
+  },
+  {
+    "revision": "28c1d77e64a9b045d7cb0918bb509e9b",
+    "url": "/Portfolio/static/media/p4.28c1d77e.png"
+  },
+  {
+    "revision": "c44d80a0b93c390e99b8210b4c46dd09",
+    "url": "/Portfolio/static/media/p5.c44d80a0.png"
+  }
+]);
